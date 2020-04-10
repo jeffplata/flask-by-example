535c757887da2916dfc0255c585a77932cae60ad
jeff plata <jeffplata@yahoo.com>
2020-04-10 17:52:59 +0800

    config setup

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
+import os
+
 from flask import Flask
+
+
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+
+# sanity check
+print('==================')
+print(os.environ['APP_SETTINGS'])
 
 
 @app.route('/')
